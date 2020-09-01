87d76262806313df887bffd3e209d1508adc35d1
DaniloOPinheiro <danilopinheiro88@icloud.com>
2020-09-01 11:54:17 -0300

    Exercicios Desafios GFT

---
+++ Exercicio-02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercicio_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+            p.Nome = "Danilo";
+            p.Endereço = "Embu-guaçu";
+            p.Telefone = "(11)9876-5432";
+            
+
+            Console.WriteLine($"Nome : {p.Nome}, Endereço : {p.Endereço}, Telefone : {p.Telefone}");
+        }
+
+        public class Pessoa
+        {
+            public string Nome { get; set; }
+            public string Endereço { get; set; }
+            public string Telefone { get; set; }
+        }
+    }
+}
