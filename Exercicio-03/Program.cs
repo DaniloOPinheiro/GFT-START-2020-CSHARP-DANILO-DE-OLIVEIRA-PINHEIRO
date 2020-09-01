36e39f3f68e11014a05923013b7d41045469b5ca
DaniloOPinheiro <danilopinheiro88@icloud.com>
2020-09-01 16:59:35 -0300

    Refeito

--- Exercicio-03/Program.cs
+++ Exercicio-03/Program.cs
@@ -6,36 +6,47 @@ namespace Exercicio_03
     {
         static void Main(string[] args)
         {
-            ContaCorrente cc = new ContaCorrente();
-            
-
-            ContaPoupanca cp = new ContaPoupanca();
+            // var primeiraConta = new Conta
+            // {
+            //     Numero = 123,
+            //     Titulo = "Pessoa",
+            //     Saldo = 100.0
+            // };
+            // Console.WriteLine(primeiraConta.Numero + primeiraConta.Titulo);
+            // Console.ReadLine();
         }
+    }
 
-        public abstract class Conta
+    public abstract class Conta
+    {
+        public int Numero { get; set; }
+        public string Titulo { get; set; }
+        public double Saldo { protected get; set; }
+
+        public abstract double Rendiemnto(double valor, double saldo);
+    }
+
+    public class ContaCorrente : Conta
+    {
+        public override double Rendiemnto(double valor, double saldo)
         {
-            int numero;
-            string titulo;
-            double saldo;
-
-           public abstract void Rendiemnto();
-
-        }
-
-        public class ContaCorrente : Conta
-        {
-            public override void Rendiemnto()
+            if (saldo  >= valor)
             {
-                saldo * 0.03;
+                saldo -= (valor * 0.3);
             }
+            return saldo;
         }
+    }
 
-        public class ContaPoupanca : Conta
+    public class ContaPoupanca : Conta
+    {
+        public override double Rendiemnto(double valor, double saldo)
         {
-            public override void Rendiemnto()
+            if (saldo  >= valor)
             {
-                saldo * 0.05;
+                saldo -= (valor * 0.5);
             }
+            return saldo;
         }
     }
 }
