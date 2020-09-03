8b7ac647da7a326881286eca1bdcbc2cdddc84e6
Danilo.O.Pinheiro <52793184+DaniloOPinheiro@users.noreply.github.com>
2020-09-03 12:02:55 -0300

    Update Program.cs

--- Exercicio-01/Program.cs
+++ Exercicio-01/Program.cs
@@ -11,13 +11,6 @@ namespace Exercicio_01
             Console.WriteLine("Introduza outro numero");
             var b = Int32.Parse(Console.ReadLine());
 
-            if (b < a)
-            {
-                var c = a;
-                a = b;
-                b = c;
-            }
-
             for (int i = ++a; i < b; i++)
             {
                  if( i % 2 != 0 )
